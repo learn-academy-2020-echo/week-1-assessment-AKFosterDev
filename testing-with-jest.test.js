78772e250a000d94a9741e541f5a3b5888d0374c
AKFosterDev <akfoster2015@gmail.com>
2020-11-07 10:42:37 -0800

    complete testing assessment

--- testing-with-jest.test.js
+++ testing-with-jest.test.js
@@ -1,8 +1,5 @@
 // ASSESSMENT 1: Testing practical questions
 
-const { expect } = require('@jest/globals');
-const { describe } = require('yargs');
-
 // $ yarn add jest
 
 // Practice red-green refactor development
@@ -10,36 +7,32 @@ const { describe } = require('yargs');
 
 // --------------------1a) Create a test for a function called addThemUp that takes two numbers as arguments and returns the sum.
 
-// describe('addThemUp', () => {
-//   test('takes in two numbers as arguments and returns the sum'),
-//     () => {
-//       expect(addThemUp(1, 2)).toEqual(3);
-//     };
-// });
+describe('addThemUp', () => {
+	test('takes in two numbers as arguments and returns the sum', () => {
+		expect(addThemUp(1, 2)).toEqual(3)
+		expect(addThemUp(9, 11)).toEqual(20)
+		expect(addThemUp(125, 250)).toEqual(375)
+	})
+})
 
-// // --------------------1b) See the test fail. THEN write the code to make the test pass.
+// --------------------1b) See the test fail. THEN write the code to make the test pass.
 
-// const addThemUp = (num1, num2) => {
-//   return num1 + num2;
-// };
-
-describe('mood', () => {
-  test('it takes an argument of mood and return the appropiate action', () => {
-    expect(mood('tired')).toEqual('drink coffee');
-    expect(mood('not tired')).toEqual('keep working');
-  });
-});
-
-const mood = feeling => {
-  if (feeling === 'tired') {
-    return 'drink coffee';
-  } else if (feeling === 'not tired') {
-    return 'keep working';
-  } else {
-    return 'think about it';
-  }
-};
+const addThemUp = (num1, num2) => {
+	return num1 + num2
+}
 
 // --------------------2a) Create a test for a function called triangleArea that takes the base and the height of a triange and returns the area. The area of a triange is the base times the height divided by two.
 
+describe('triangleArea', () => {
+	test('takes the base and the height of a triangle and returns the area', () => {
+		expect(triangleArea(2, 4)).toEqual(4)
+		expect(triangleArea(5, 10)).toEqual(25)
+		expect(triangleArea(25, 100)).toEqual(1250)
+	})
+})
+
 // --------------------2b) See the test fail. THEN write the code to make the test pass.
+
+const triangleArea = (num1, num2) => {
+	return (num1 * num2) / 2
+}
